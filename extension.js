3715c49eabe6d38eae995e7750e90f79600e4b90
Roberto Asiel Guevara Castañeda <raguevara@uci.cu>
2016-09-06 01:33:56 -0400

    Added support for editor and explorer context menu

--- extension.js
+++ extension.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var fs = require('fs');
+var path = require('path');
 var vscode = require('vscode');
 var fileService = require('./libs/fileService');
 
@@ -16,57 +18,96 @@ var openController = (function openControllerIIFE() {
         var opnOptionObj = config.perLang.opnOptions[i];
 
         if (activeTextEditor.document.languageId === opnOptionObj.forLang) {
-
           fileService.openFileLocation(fileService.getFileLocation(activeTextEditor, config, opnOptionObj), opnOptionObj);
           break;
-
         }
 
       }
 
     } else {
-
       fileService.openFileLocation(fileService.getFileLocation(activeTextEditor));
     }
 
   };
 
-  var openFile = function openUriAnonFn() {
+  var activeEditor = function activeEditor(editor) {
 
-    var editor = vscode.window.activeTextEditor;
-
-    if (!editor || !editor.document.uri) {
-
+		if (!editor || !editor.document.uri) {
       vscode.window.showInformationMessage('No active editor or URI available');
       return;
-
     }
 
-    openFileWithOptions(editor);
+		openFileWithOptions(editor);
 
+	};
+
+  var openFile = function openFile(filePath) {
+
+		if (filePath) {
+
+			var ext = path.extname(filePath);
+			var filename = path.basename(filePath, ext);
+
+			vscode.window.showTextDocument({
+				fileName: filename,
+				uri: vscode.Uri.parse('file:///' + filePath)
+			}).then(activeEditor);
+
+		} else {
+    	var editor = vscode.window.activeTextEditor;
+			activeEditor(editor);
+		}
   };
 
-  var disposable = vscode.commands.registerCommand('extension.opn', function openUriAnonFn() {
+	var normalizePath = function normalizePath(dir) {
+
+    if (process.platform === 'win32') {
+			dir = dir.replace(/\\/g, '/');
+    }
+    return dir;
+
+	};
+
+	var verifyFileFromPath = function verifyFileFromPath(path) {
+
+    var dir = fs.statSync(path);
+    path = normalizePath(path);
+
+    if (dir.isFile()) {
+			return path;
+    } else {
+			return null;
+		}
+
+	};
+
+  var disposable = vscode.commands.registerCommand('extension.opn', function openUriAnonFn(uri) {
+
+		var same = false;
+		var filePath = null;
+		var active = vscode.window.activeTextEditor;
+
+		if (active) {
+			same = vscode.window.activeTextEditor.document.uri.fsPath === uri.fsPath;
+		}
+
+		if (uri && !same) {
+			filePath = verifyFileFromPath(uri.fsPath);
+		}		
 
     try {
-
-      openFile();
-
+      openFile(filePath);
     }
     catch (error) {
-
       vscode.window.showInformationMessage('Error! Could not open file.');
       console.error(error.stack);
-
     }
 
   });
 
   var executeOpen = function executeOpenAnonFn() {
-
     var subscriptions = [];
     subscriptions.push(disposable);
-
   };
 
   return {
@@ -76,7 +117,6 @@ var openController = (function openControllerIIFE() {
 })();
 
 function activate(context) {
-
   var controller = openController.executeOpen();
   context.subscriptions.push(controller);
 }
@@ -88,4 +128,3 @@ function deactivate() {
 }
 
 exports.deactivate = deactivate;
-
